6f8acc326b850e63e86fca22c911f434c1b19949
Colin Worf and Holly Wilkalis <holly@>
2017-10-24 14:19:32 -0700

    add questions and script for second set of questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 $(function() {
   $("form#transportation").submit(function(event) {
     event.preventDefault();
+
     $("#work-responses").show();
     $("input:checkbox[name=work]:checked").each(function(){
       var workMode = $(this).val();
       $("#work-responses").append(workMode + "<br>");
+    });
+
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun]:checked").each(function(){
+      var funMode = $(this).val();
+      $("#fun-responses").append(funMode + "<br>");
     });
     $("#transportation").hide();
   });
